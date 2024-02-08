bc895cc0633f76dd3e06627bce3ae523b4ae9c0f
fschmuck <flou501@gmail.com>
2024-02-08 09:19:15 +0100

    [API] - Create new posts

--- application/src/main/kotlin/lv/webtech/demo/DemoController.kt
+++ application/src/main/kotlin/lv/webtech/demo/DemoController.kt
@@ -1,10 +1,12 @@
 package lv.webtech.demo
 
 import lv.webtech.demo.openapi.api.PostsApi
+import lv.webtech.demo.openapi.model.CreatePostRequest
 import lv.webtech.demo.openapi.model.Post
 import lv.webtech.demo.repository.PostsRepository
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -23,5 +25,10 @@ class DemoController(val postsRepository: PostsRepository) : PostsApi {
         }.toList()
         return ResponseEntity.ok(posts)
     }
-
+    @PostMapping
+    override fun createPosts(createPostRequest: CreatePostRequest): ResponseEntity<Void> {
+        val createPost = lv.webtech.demo.entity.Post(null, createPostRequest.title, createPostRequest.message)
+        postsRepository.save(createPost)
+        return ResponseEntity.ok().build()
+    }
 }
